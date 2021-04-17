3f76176eb0118d94d1da44649456bc5bf86b1e8e
owenThurm <owen.p.thurm@gmail.com>
2021-04-17 00:17:21 -0400

    Render CSS inside iframe

--- src/Container.js
+++ src/Container.js
@@ -17,13 +17,24 @@ const DndFrame = ({ children }) => {
 
 // Don't use the decorator, embed the DnD context within the iframe
 export const Container = ({content, addContent}) => {
+
+  const renderHead = () => {
+    // const {styleDependencies} = this.props
+    // {styleDependencies.map(url => <link rel="stylesheet" href={url} />)}
+    return (
+      <React.Fragment>
+        <div dangerouslySetInnerHTML={{__html: document.head.innerHTML}} />
+        <link rel="stylsheet" href="index.css"/>
+      </React.Fragment>
+    )
+  }
     // The react-frame-component will pass the iframe's 'window' global as a context value
     // to the DragDropContext provider. You could also directly inject it in via a prop.
     // If neither the prop or the context value for 'window' are present, the DndProvider
     // will just use the global window.
     return (
       <>
-        <Frame style={{ width: '100%', height: 400 }}>
+        <Frame head={renderHead()} style={{ width: '100%', height: 400 }}>
           <DndFrame>
             <div>
               <div style={{ overflow: 'hidden', clear: 'both' }}>
