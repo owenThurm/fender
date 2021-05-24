52f804833f4b35dc7c381e7f9e67a75d1162a864
owenThurm <owen.p.thurm@gmail.com>
2021-05-23 23:14:11 -0400

    Reformat

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,6 @@
 import { Card } from 'antd';
 
-export default ({className, position, size, style, setShowStyleMenu}) => (
+export default ({ className, position, size, style, setShowStyleMenu }) => (
   <Card
     className={className}
     style={{position: 'absolute', left: position.x, top: position.y, width: size.width, height: size.height, ...style}}
