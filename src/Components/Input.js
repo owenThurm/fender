d998c4c0b459f086b3fe4198b5891cbd333b1e08
owenThurm <owen.p.thurm@gmail.com>
2021-05-30 15:30:22 -0400

    ESlintify Input component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -2,13 +2,32 @@ import { Input } from 'antd';
 import React from 'react';
 import { componentPropTypes, componentDefaultProps } from '../Types/Types';
 
-export default ({ className, position, size, setShowStyleMenu, setShowMoveable, style }) => (
-<Input
-  className={className}
-  style={{position: 'absolute', left: position.x, top: position.y, height: size.height, width: size.width, ...style}}
-  placeholder='input here'
-  onClick={() => setShowMoveable(true)}
-  onDoubleClick={() => setShowStyleMenu(true)}
+const InputComponent = ({
+  className,
+  position: { x, y },
+  size: { width, height },
+  setShowStyleMenu,
+  setShowMoveable,
+  style,
+}) => (
+  <Input
+    className={className}
+    style={{
+      position: 'absolute',
+      left: x,
+      top: y,
+      height,
+      width,
+      ...style,
+    }}
+    placeholder="input here"
+    onClick={() => setShowMoveable(true)}
+    onDoubleClick={() => setShowStyleMenu(true)}
   />
-)
+);
 
+InputComponent.propTypes = componentPropTypes;
+
+InputComponent.defaultProps = componentDefaultProps;
+
+export default InputComponent;
