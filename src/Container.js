94c724c97e074ba9f8c4c202f867e9e50b1a36d7
owenThurm <owen.p.thurm@gmail.com>
2021-05-30 16:43:36 -0400

    ESlintify Container.js

--- src/Container.js
+++ src/Container.js
@@ -1,8 +1,9 @@
 import { DndContext } from 'react-dnd';
-import { Dustbin } from './Dustbin';
-import Frame from 'react-frame-component';
 import React, { useContext, useEffect } from 'react';
 import { FrameContext } from 'react-frame-component';
+import PropTypes from 'prop-types';
+import Dustbin from './Dustbin';
+import Block from './Models/Block';
 
 const DndFrame = ({ children }) => {
   const { dragDropManager } = useContext(DndContext);
@@ -15,32 +16,35 @@ const DndFrame = ({ children }) => {
   return children;
 };
 
-// Don't use the decorator, embed the DnD context within the iframe
-export const Container = ({content, addContent, editContent}) => {
-
-  const renderHead = () => {
-    // const {styleDependencies} = this.props
-    // {styleDependencies.map(url => <link rel="stylesheet" href={url} />)}
-    return (
-      <React.Fragment>
-        <div dangerouslySetInnerHTML={{__html: document.head.innerHTML}} />
-        <link rel="stylsheet" href="index.css"/>
-      </React.Fragment>
-    )
-  }
-    // The react-frame-component will pass the iframe's 'window' global as a context value
-    // to the DragDropContext provider. You could also directly inject it in via a prop.
-    // If neither the prop or the context value for 'window' are present, the DndProvider
-    // will just use the global window.
-    return (
-      <div class="outside"><DndFrame>
-
-            <div class="container" style={{width: 800}}>
-
-            <Dustbin  content={content} addContent={addContent} editContent={editContent}/>
-
-            </div>
-          </DndFrame>
-        </div>
-      );
+const containerPropTypes = {
+  content: PropTypes.arrayOf(
+    PropTypes.instanceOf(Block),
+  ).isRequired,
+  addContent: PropTypes.func.isRequired,
+  editContent: PropTypes.func.isRequired,
 };
+
+// Don't use the decorator, embed the DnD context within the iframe
+const Container = ({
+  content,
+  addContent,
+  editContent,
+}) => (
+  // The react-frame-component will pass the iframe's 'window' global as a context value
+  // to the DragDropContext provider. You could also directly inject it in via a prop.
+  // If neither the prop or the context value for 'window' are present, the DndProvider
+  // will just use the global window.
+  <div>
+    <DndFrame>
+      <div
+        style={{ width: 800 }}
+      >
+        <Dustbin content={content} addContent={addContent} editContent={editContent} />
+      </div>
+    </DndFrame>
+  </div>
+);
+
+Container.propTypes = containerPropTypes;
+
+export default Container;
