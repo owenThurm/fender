620ecf739fec46934b6f1baca3131a060781d735
owenThurm <owen.p.thurm@gmail.com>
2021-05-30 15:22:01 -0400

    Remove CenteringLine

--- src/Components/CenteringLine.js
+++
@@ -1,8 +0,0 @@
-export default ({setShowCenteringLines}) => (
-    <CenteringLine
-      style={{position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', ...style}}
-      onDoubleClick={() => setShowStyleMenu(true)}
-      >
-      Click Me
-    </CenteringLine>
-    )
